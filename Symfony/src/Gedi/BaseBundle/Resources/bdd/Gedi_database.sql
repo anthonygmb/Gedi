379aaeb6055bde71e2aa409101945cc046c3b503
explorer <anthonygumb.dev@gmail.com>
2017-03-14 19:57:56 +0100

    ajout du logo r2d2 gedi

--- Symfony/src/Gedi/BaseBundle/Resources/bdd/Gedi_database.sql
+++ Symfony/src/Gedi/BaseBundle/Resources/bdd/Gedi_database.sql
@@ -1,11 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Client :  127.0.0.1
--- Généré le :  Mer 16 Novembre 2016 à 11:59
--- Version du serveur :  10.1.19-MariaDB
--- Version de PHP :  5.6.24
+-- ==========================
+-- Base de données : GEDI
+-- ==========================
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,10 +9,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
--- ==========================
--- Base de données : GEDI
--- ==========================
 
 -- ====================================================================================================
 -- Structure de la table Utilisateur
